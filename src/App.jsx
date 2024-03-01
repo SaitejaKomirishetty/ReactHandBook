17d5c87a274d3653ad368cf841c30223432ea105
SAITEJA KOMIRISHETTY <komirishettysaiteja@gmail.com>
2024-03-02 00:51:53 +0530

    testing new branching strat

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/start' element={<Start />} />
+        <Route path='/new' element={<>New Route</>} />
       </Routes>
     </>
   );
